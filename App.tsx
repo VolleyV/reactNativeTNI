992e7d8aad4299596040ab5a8c028e177aa33f73
VolleyV <nopphoom@gmail.com>
2024-07-13 11:09:49 +0700

    BasicRactnative update

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View ,Button,Alert} from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import AppFooter from './components/AppFooter';
+import AppHeader from './components/AppHeader';
 
-export default function App() {
+export default function App():React.JSX.Element {
+  const onClickme=()=>{
+    Alert.alert("Hi","React Native is Fun!")
+  }
+  const users=[
+    {id:1001,name:"Pete"},
+    {id:1002,name:"Fred"}
+  ]
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <AppHeader title="This is Header" year={2018}/>
+      <AppHeader title="This is a Header 2"/>
+      <Text>Hello React Native!</Text>
+      {users.map((data,index)=>{
+        return(
+          <Text key={data.id}>
+          No.{index+1}, ID: {data.id}, Name: {data.name}
+          </Text>
+        )
+        })}
+      <Button title="Click Me" 
+        onPress={onClickme}
+        color={"#45CCCA"}
+        /* onPress={()=>
+        {Alert.alert("Hi","React Native is Fun!")}}  */
+       />
       <StatusBar style="auto" />
+      <AppFooter/>
     </View>
   );
 }
