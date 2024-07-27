11000e641ea79e7ea55a44b65672cfa2862e25e1
VolleyV <nopphoom@gmail.com>
2024-07-27 11:53:00 +0700

    react Hook 27/7/2024

--- components/ProfileScreen.tsx
+++ components/ProfileScreen.tsx
@@ -1,19 +1,33 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import {StyleSheet,Text, View, Image, Button } from "react-native";
 import styles from "../styles/style";
-import React from "react";
+import React, { useState } from "react";
 
 const ProfileScreen = (): React.JSX.Element => {
   const profileImage = require("..//assets/studentImage.jpeg");
+  const newProfileImage =require("..//assets/coppeachunknow.jpeg")
+  const [name, setName] = useState("Nopphoom Boonchan");
+  const [img,setImg]=useState(profileImage)
+  const handleChangeName = ()=> {
+    setName(name == "Nopphoom Boonchan"?"New Name":"Nopphoom Boonchan")
+  };
+  const handleChangeImage = ()=> {
+    setImg(img == profileImage?newProfileImage:profileImage)
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.profileContainer}>
-        <Image source={profileImage} style={styles.profileImage} />
-        <Text style={styles.profileName}>Nopphoom Boonchan</Text>
+        <Image source={img} style={styles.profileImage} />
+        <View style={{width:200}}>
+        <Text style={styles.profileName} >{name}</Text>
+        <Button  onPress={handleChangeName} title="Click Name" ></Button>
+        <Text>{"\n"}</Text>
+        <Button onPress={handleChangeImage} title="Click Image"></Button>
+        </View>
       </View>
+      
     </View>
   );
 };
 
 export default ProfileScreen;
-
--- styles/style.ts
+++ styles/style.ts
@@ -30,4 +30,4 @@ const styles = StyleSheet.create({
     }
   });
 
-  export default styles;
+  export default styles
